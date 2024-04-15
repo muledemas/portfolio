8c3fd5c48982d7b126527cf001dc247acaf71b7f
Muluken Demas <mulerian@gmail.com>
2024-04-15 12:27:42 +0300

    download checked

--- src/components/experience/Experiance.js
+++ src/components/experience/Experiance.js
@@ -1,167 +1,165 @@
 import { Component } from "react";
-import styles from './experience.module.css'
+import styles from "./experience.module.css";
 
-const Experiance=(props)=>{
-    return (
-      <section id={styles.experiance} name="experience" ref={props.refs}>
-        <p className={styles.sectionTextP1}>Explore My</p>
-        <h1 className={styles.title}>Skills</h1>
-        <div className={styles.experienceDetailsContainer}>
-          <div className={styles.aboutContainers}>
-            <div className={styles.detailsContainer}>
-              <h2 className={styles.experienceSubTitle}>
-                Frontend Development
-              </h2>
-              <div className={styles.articleContainer}>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>HTML</h3>
-                    <p>Experienced</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>CSS</h3>
-                    <p>Intermediate</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>Angular</h3>
-                    <p>Experienced</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>JavaScript</h3>
-                    <p>Experienced</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>React</h3>
-                    <p>Experienced</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>Material UI</h3>
-                    <p>Intermediate</p>
-                  </div>
-                </article>
-              </div>
+const Experiance = (props) => {
+  return (
+    <section id={styles.experiance} name="experience" ref={props.refs}>
+      <p className={styles.sectionTextP1}>Explore My</p>
+      <h1 className={styles.title}>Skills</h1>
+      <div className={styles.experienceDetailsContainer}>
+        <div className={styles.aboutContainers}>
+          <div className={styles.detailsContainer}>
+            <h2 className={styles.experienceSubTitle}>Frontend Development</h2>
+            <div className={styles.articleContainer}>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>HTML</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>CSS</h3>
+                  <p>Intermediate</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>Angular</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>JavaScript</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>React</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>Material UI</h3>
+                  <p>Intermediate</p>
+                </div>
+              </article>
             </div>
-            <div className={styles.detailsContainer}>
-              <h2 className={styles.experienceSubTitle}>Tools</h2>
-              <div className={styles.articleContainer}>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>Docker</h3>
-                    <p>Basic</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>Figma</h3>
-                    <p>Intermediate</p>
-                  </div>
-                </article>
+          </div>
+          <div className={styles.detailsContainer}>
+            <h2 className={styles.experienceSubTitle}>Tools</h2>
+            <div className={styles.articleContainer}>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>Docker</h3>
+                  <p>Basic</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>Figma</h3>
+                  <p>Intermediate</p>
+                </div>
+              </article>
 
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>Jira</h3>
-                    <p>Intermediate</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>Git</h3>
-                    <p>Intermediate</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>Keycloak</h3>
-                    <p>Intermediate</p>
-                  </div>
-                </article>
-                <article>
-                  <img
-                    src="./assets/images/checkmark.png"
-                    alt="Experience icon"
-                    className={styles.icon}
-                  />
-                  <div>
-                    <h3>Camunda</h3>
-                    <p>Intermediate</p>
-                  </div>
-                </article>
-              </div>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>Jira</h3>
+                  <p>Intermediate</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>Git</h3>
+                  <p>Intermediate</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>Keycloak</h3>
+                  <p>Intermediate</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src="./assets/images/checkmark.png"
+                  alt="Experience icon"
+                  className={styles.icon}
+                />
+                <div>
+                  <h3>Camunda</h3>
+                  <p>Intermediate</p>
+                </div>
+              </article>
             </div>
           </div>
         </div>
-        <img
-          src="./assets/images/arrow.png"
-          alt="Arrow icon"
-          className={styles.arrow}
-          onClick={props.scrollToSection}
-        />
-      </section>
-    );
-}
-export default Experiance
+      </div>
+      <img
+        src="./assets/images/arrow.png"
+        alt="Arrow icon"
+        className={styles.arrow}
+        onClick={props.scrollToSection}
+      />
+    </section>
+  );
+};
+export default Experiance;
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,6 +2,18 @@ import { Component } from "react";
 import styles from './profile.module.css';
 
 const Profile = ()=>{
+   const downloadFile = () => {
+     // Logic to trigger file download
+      const filePath = process.env.PUBLIC_URL + "/assets/images/resume.pdf";
+      const link = document.createElement("a");
+      link.href = filePath;
+      link.setAttribute("download", "resume.pdf");
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+     //window.location.href = "./assets/images/resume.pdf";
+   };
     return (
       <section id={styles.profile} name="profile">
         <div className={styles.sectionText}>
@@ -9,8 +21,11 @@ const Profile = ()=>{
           <h1 className={styles.title}>Muluken Demas</h1>
           <p className={styles.sectionTextP2}>Frontend Developer</p>
           <div className={styles.btnContainer}>
-            <button className={styles.btn + " " + styles.btnColor2}>
-            Resume
+            <button
+              className={styles.btn + " " + styles.btnColor2}
+              onClick={downloadFile}
+            >
+              Resume
             </button>
             <button className={styles.btn + " " + styles.btnColor1}>
               Contact Info
